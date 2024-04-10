e5f6b20295aa3be828aa13c504f6f85e5abbce15
Arielle Leon <ariellel@alleninstitute.org>
2024-04-10 13:26:09 -0700

    adding comments

--- src/aind_watchdog_service/alert_bot.py
+++ src/aind_watchdog_service/alert_bot.py
@@ -3,7 +3,7 @@
 from typing import Optional
 
 import requests
-
+# From Alessio Buccino - pulled from aind-codeocean-utils 
 
 class AlertBot:
     """Class to handle sending alerts and messages in MS Teams."""
--- src/aind_watchdog_service/trigger_job.py
+++ src/aind_watchdog_service/trigger_job.py
@@ -8,6 +8,7 @@ import aind_data_transfer_service
 
 
 def check_files(event: str, modality_src: dict) -> bool:
+    
     pass
 
 
