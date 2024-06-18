e49ce8a8159c0b484ec0de08e118d75ba170ce44
Arielle Leon <ariellel@alleninstitute.org>
2024-06-18 10:29:11 -0700

    adding validation check

--- src/aind_watchdog_service/models/manifest_config.py
+++ src/aind_watchdog_service/models/manifest_config.py
@@ -5,7 +5,7 @@ from typing import Dict, List, Literal, Optional
 
 from aind_data_schema_models.modalities import Modality
 from aind_data_schema_models.platforms import Platform
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, field_validator
 
 
 class ManifestConfig(BaseModel):
@@ -30,9 +30,12 @@ class ManifestConfig(BaseModel):
         description="Transfer time to schedule copy and upload. If None defaults to trigger the transfer immediately",  # noqa
         title="APScheduler transfer time",
     )
-    platform: Platform.ONE_OF = Field(description="Platform type", title="Platform type")
+    platform: str = Field(description="Platform type", title="Platform type")
     capsule_id: Optional[str] = Field(
         ..., description="Capsule ID of pipeline to run", title="Capsule"
+    )
+    mount: Optional[str] = Field(
+        ..., description="Mount point for pipeline run", title="Mount point"
     )
     s3_bucket: Optional[Literal["s3", "public", "private", "scratch"]] = Field(
         default=None, description="s3 endpoint", title="S3 endpoint"
@@ -44,7 +47,7 @@ class ManifestConfig(BaseModel):
         description="where to send data to on VAST",
         title="VAST destination and maybe S3?",
     )
-    modalities: Dict[Modality.ONE_OF, List[str]] = Field(
+    modalities: Dict[str, List[str]] = Field(
         default={},
         description="list of ModalityFile objects containing modality names and associated files or directories",  # noqa
         title="modality files",
@@ -57,3 +60,23 @@ class ManifestConfig(BaseModel):
     script: Dict[str, List[str]] = Field(
         default={}, description="Set of commands to run in subprocess.", title="Commands"
     )
+
+    @field_validator("platform")
+    @classmethod
+    def check_platform_string(cls, input_platform: str) -> str:
+        """Checks if str can be converted to platform model"""
+        if input_platform in Platform._abbreviation_map:
+            return input_platform
+        else:
+            raise AttributeError(f"Unknown platform: {input_platform}")
+
+    @field_validator("modalities")
+    @classmethod
+    def check_modality_string(
+        cls, input_modality: Dict[str, List[str]]
+    ) -> Dict[str, List[str]]:
+        """Checks if str can be converted to platform model"""
+        for key in input_modality.keys():
+            if key not in Modality._abbreviation_map:
+                raise AttributeError(f"Unknown modality: {input_modality}")
+        return input_modality
