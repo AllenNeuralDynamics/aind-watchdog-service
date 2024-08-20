0f14f5e9287c96911c253530a6b0583e16b0ec39
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2024-08-19 17:18:11 -0700

    Linting tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,12 +1,8 @@
 import unittest
-from datetime import datetime as dt
 from pathlib import Path
 
-import pydantic_core
 import yaml
-from pydantic import ValidationError
 
-from aind_watchdog_service.models.manifest_config import ManifestConfig
 from aind_watchdog_service.models.watch_config import WatchConfig
 
 TEST_DIRECTORY = Path(__file__).resolve().parent
