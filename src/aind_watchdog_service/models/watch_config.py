f09dd7bbec452d3cb8e4af09c0f483cc9e4dfdd8
Patrick Latimer <patrick.latimer@alleninstitute.org>
2024-09-27 15:14:52 -0700

    Change type hint to support python 3.9

--- src/aind_watchdog_service/models/watch_config.py
+++ src/aind_watchdog_service/models/watch_config.py
@@ -1,6 +1,6 @@
 """ Configuration for watchdog service"""
 
-from typing import Optional
+from typing import Optional, Union
 
 from pydantic import BaseModel, Field
 
@@ -21,7 +21,7 @@ class WatchConfig(BaseModel):
         description="Teams webhook url for user notification",
         title="Teams webhook url",
     )
-    misfire_grace_time_s: int | None = Field(
+    misfire_grace_time_s: Union[int, None] = Field(
         default=3 * 3600,
         description="If the job scheduler is busy, wait this long before skipping a job."
         + " If None, allow the job to run no matter how late it is",
