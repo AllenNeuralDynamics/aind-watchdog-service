180e0d5f426240db27ac8ea2d620aedd82160731
arielleleon <54869022+arielleleon@users.noreply.github.com>
2024-10-03 11:00:55 -0700

    65 manifest fail to load error (#72)
    Co-authored-by: brunocruz <7049351+bruno-f-cruz@users.noreply.github.com>

--- src/aind_watchdog_service/event_handler.py
+++ src/aind_watchdog_service/event_handler.py
@@ -1,15 +1,21 @@
 """Event handler module"""
 
+import datetime
 import logging
-from datetime import datetime as dt
-from datetime import time, timedelta
+import time
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Union
 
 import yaml
-from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.job import Job
-from watchdog.events import FileCreatedEvent, FileSystemEventHandler
+from apscheduler.schedulers.background import BackgroundScheduler
+from watchdog.events import (
+    DirCreatedEvent,
+    DirDeletedEvent,
+    FileCreatedEvent,
+    FileDeletedEvent,
+    FileSystemEventHandler,
+)
 
 from aind_watchdog_service.models.manifest_config import ManifestConfig
 from aind_watchdog_service.models.watch_config import WatchConfig
@@ -25,21 +31,31 @@ class EventHandler(FileSystemEventHandler):
         self.scheduler = scheduler
         self.config = config
         self.jobs: Dict[str, Job] = {}
+        self._startup_manifest_check()
 
-    def _load_manifest(self, event: FileCreatedEvent) -> ManifestConfig:
+    def _startup_manifest_check(self) -> None:
+        """ " Check for manifests to process in the manifest directory on startup"""
+        manifest_dir = Path(self.config.flag_dir).glob("*manifest*.*")
+        for manifest in manifest_dir:
+            transfer_config = self._load_manifest(str(manifest))
+            if transfer_config:
+                self.schedule_job(str(manifest), transfer_config)
+
+    def _load_manifest(self, src_path: str) -> ManifestConfig:
         """Instructions to transfer to VAST
 
         Parameters
         ----------
-        event : FileCreatedEvent
-           file modified event
+        src_path : str
+            manifest file path to trigger
 
         Returns
         -------
         dict
            manifest configuration
         """
-        with open(event.src_path, "r", encoding="utf-8") as f:
+        logging.info("Loading manifest %s", src_path)
+        with open(src_path, "r", encoding="utf-8") as f:
             try:
                 data = yaml.safe_load(f)
                 config = ManifestConfig(**data)
@@ -47,7 +63,7 @@ class EventHandler(FileSystemEventHandler):
                 logging.error("Error loading config %s", repr(e))
         return config
 
-    def _get_trigger_time(self, transfer_time: time) -> dt:
+    def _get_trigger_time(self, transfer_time: datetime.time) -> datetime.datetime:
         """Get trigger time from the job
 
         Parameters
@@ -56,45 +72,47 @@ class EventHandler(FileSystemEventHandler):
             time to trigger the job
         Returns
         -------
-        dt
+        datetime.datetime
             datetime object
         """
-        _now = dt.now()
-        trigger_time = dt.combine(_now.date(), transfer_time)
+        _now = datetime.datetime.now()
+        trigger_time = datetime.datetime.combine(_now.date(), transfer_time)
         trigger_time = (
-            trigger_time if trigger_time > _now else trigger_time + timedelta(days=1)
+            trigger_time
+            if trigger_time > _now
+            else trigger_time + datetime.timedelta(days=1)
         )
         logging.info("Trigger time %s", trigger_time)
         return trigger_time
 
-    def schedule_job(self, event: FileCreatedEvent, job_config: ManifestConfig) -> None:
+    def schedule_job(self, src_path: str, job_config: ManifestConfig) -> None:
         """Schedule job to run
 
         Parameters
         ----------
-        event : FileCreatedEvent
-            event to trigger job
+        src_path : str
+            manifest file path to trigger
         config : dict
             configuration for the job
         """
         if not job_config.schedule_time:
-            logging.info("Scheduling job to run now %s", event.src_path)
-            run = RunJob(event, job_config, self.config)
+            logging.info("Scheduling job to run now %s", src_path)
+            run = RunJob(src_path, job_config, self.config)
             job_id = self.scheduler.add_job(run.run_job)
 
         else:
             trigger = self._get_trigger_time(job_config.schedule_time)
-            logging.info("Scheduling job to run at %s %s", trigger, event.src_path)
-            run = RunJob(event, job_config, self.config)
+            logging.info("Scheduling job to run at %s %s", trigger, src_path)
+            run = RunJob(src_path, job_config, self.config)
             job_id = self.scheduler.add_job(run.run_job, "date", run_date=trigger)
-        self.jobs[event.src_path] = job_id
+        self.jobs[src_path] = job_id
 
-    def on_deleted(self, event: FileCreatedEvent) -> None:
+    def on_deleted(self, event: Union[FileDeletedEvent, DirDeletedEvent]) -> None:
         """Event handler for file deleted event
 
         Parameters
         ----------
-        event : FileCreatedEvent
+        event : FileDeletedEvent | DirDeletedEvent
             file deleted event
 
         Returns
@@ -107,12 +125,12 @@ class EventHandler(FileSystemEventHandler):
             del self.jobs[event.src_path]
         logging.info("Jobs in queue %s", self.scheduler.get_jobs())
 
-    def on_created(self, event: FileCreatedEvent) -> None:
+    def on_created(self, event: Union[FileCreatedEvent, DirCreatedEvent]) -> None:
         """Event handler for file modified event
 
         Parameters
         ----------
-        event : FileCreatedEvent
+        event : FileCreatedEvent | DirCreatedEvent
             file modified event
 
         Returns
@@ -120,9 +138,10 @@ class EventHandler(FileSystemEventHandler):
         None
         """
         # Check if manifest file is being modified / created
-        if Path(event.src_path).is_dir():
+        _path = Path(str(event.src_path))
+        if isinstance(event, DirCreatedEvent) | _path.is_dir():
             return
-        if "manifest" not in event.src_path:
+        if "manifest" not in _path.name:
             return
         # If scheduled manifest is being modified, remove original job
         if event.src_path in self.jobs:
@@ -130,6 +149,7 @@ class EventHandler(FileSystemEventHandler):
             self.scheduler.remove_job(self.jobs[event.src_path].id)
             del self.jobs[event.src_path]
         logging.info("Found event file %s", event.src_path)
-        transfer_config = self._load_manifest(event)
+        time.sleep(10)  # Wait for file to be written
+        transfer_config = self._load_manifest(event.src_path)
         if transfer_config:
-            self.schedule_job(event, transfer_config)
+            self.schedule_job(event.src_path, transfer_config)
